2ddd23a530f8e00cbb0a6565f6a8e7051514e6e2
DNYANESHWAR SHINDE <85820206+create-dan@users.noreply.github.com>
2022-03-09 18:39:34 +0530

    Add files via upload

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img1.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Enter Your Details',
+                style: TextStyle(color: Colors.white, fontSize: 40.0),
+              ),
+              SizedBox(height: 200),
+              TextField()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
